3f4314d1011db559c3f4fe884bc6f877ec5bc595
viet-nguyen-izundo <viet.nguyen@izundo.com>
2023-05-19 16:24:46 +0700

    try this

--- src/pages/Login/loginJWT.tsx
+++ src/pages/Login/loginJWT.tsx
@@ -69,6 +69,7 @@ const LoginJWT = ({
               )}
             />
           </Box>
+          try this
           <Box mt={3}>
             <Box style={{ display: "flex", marginBottom: "5px" }}>
               <Icons.LockIcon />
